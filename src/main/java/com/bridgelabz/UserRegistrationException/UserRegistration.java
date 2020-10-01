8210a849d255c5f440e7635240ab6ccb3ded1130
akashgupta3852 <akashgupta3852@gmail.com>
2020-10-01 12:53:02 +0530

    [Akash]Added: valiadation of last name

--- src/main/java/com/bridgelabz/UserRegistrationException/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistrationException/UserRegistration.java
@@ -4,15 +4,24 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 
-	private static final String FIRST_NAME_PATTERN="^[A-Z][a-z]{2,}";
+	private static final String NAME_PATTERN="^[A-Z][a-z]{2,}";
 	
 	public boolean checkFirstName(String fName) throws UserRegistrationException {
 		if(fName==null)
 			throw new UserRegistrationException(UserRegistrationException.ExceptionType.ENTERED_NULL ,"Enter Proper Mood");
 		if(fName.length()==0)
 			throw new UserRegistrationException(UserRegistrationException.ExceptionType.ENTERED_EMPTY ,"Enter Proper Mood");
-	    Pattern pattern = Pattern.compile(FIRST_NAME_PATTERN);
+	    Pattern pattern = Pattern.compile(NAME_PATTERN);
 	    	return pattern.matcher(fName).matches();
+    }
+	
+	public boolean checkLastName(String lName) throws UserRegistrationException {
+		if(lName==null)
+			throw new UserRegistrationException(UserRegistrationException.ExceptionType.ENTERED_NULL ,"Enter Proper Mood");
+		if(lName.length()==0)
+			throw new UserRegistrationException(UserRegistrationException.ExceptionType.ENTERED_EMPTY ,"Enter Proper Mood");
+	    Pattern pattern = Pattern.compile(NAME_PATTERN);
+	    	return pattern.matcher(lName).matches();
     }
 	
     public void printWelcome() {
--- src/test/java/com/bridgelabz/UserRegistrationException/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/UserRegistrationException/UserRegistrationTest.java
@@ -33,24 +33,80 @@ public class UserRegistrationTest {
 	}
 	
 	@Test
-	public void givenFirstName_WhenNull_ShouldReturnFalse() {
+	public void givenFirstName_WhenNull_ShouldReturnEnterProperMood() {
 		 UserRegistration userRegistration = new UserRegistration();
 		 try {
 	    	 boolean result = userRegistration.checkFirstName(null);
+	    	 Assert.assertEquals(false,result);
 	    }catch(UserRegistrationException e) {
 			Assert.assertEquals(UserRegistrationException.ExceptionType.ENTERED_NULL, e.exceptionType);
+			Assert.assertEquals("Enter Proper Mood", e.getMessage());
 	    }
-	 }
+	}
 	
 	@Test
-	public void givenFirstName_WhenEmpty_ShouldReturnFalse() {
+	public void givenFirstName_WhenEmpty_ShouldReturnEnterProperMood() {
 	UserRegistration userRegistration = new UserRegistration();
 		try {
 		    boolean result = userRegistration.checkFirstName("");
+		    Assert.assertEquals(false,result);
 		}catch(UserRegistrationException e) {
 			Assert.assertEquals(UserRegistrationException.ExceptionType.ENTERED_EMPTY, e.exceptionType);
+			Assert.assertEquals("Enter Proper Mood", e.getMessage());			
 		}
-	 }
+	}
+	
+	@Test
+	public void givenLastName_WhenProper_ShouldReturnTrue () throws UserRegistrationException {
+		UserRegistration userRegistration =  new UserRegistration();
+		boolean result = userRegistration.checkLastName("Gupta");
+		Assert.assertEquals(true,result);
+	}
+	
+	@Test
+	public void givenLastName_WhenWithSpChars_ShouldReturnFalse() throws UserRegistrationException {
+		UserRegistration userRegistration =  new UserRegistration();
+		boolean result = userRegistration.checkLastName("Gupt@");
+		Assert.assertEquals(false,result);
+	}
+	
+	@Test
+	public void givenLastName_WhenShort_ShouldReturnFalse() throws UserRegistrationException {
+		UserRegistration userRegistration =  new UserRegistration();
+		boolean result = userRegistration.checkLastName("Gu");
+		Assert.assertEquals(false,result);
+	}
+	
+	@Test
+	public void givenLastName_WhenWithNumericChars_ShouldReturnFalse() throws UserRegistrationException {
+		UserRegistration userRegistration =  new UserRegistration();
+		boolean result = userRegistration.checkLastName("Gupta123");
+		Assert.assertEquals(false,result);
+	}	
+	
+	@Test
+	public void givenLastName_WhenNull_ShouldReturnEnterProperMood() {
+		 UserRegistration userRegistration = new UserRegistration();
+		 try {
+	    	 boolean result = userRegistration.checkLastName(null);
+	    	 Assert.assertEquals(false,result);
+	    }catch(UserRegistrationException e) {
+			Assert.assertEquals(UserRegistrationException.ExceptionType.ENTERED_NULL, e.exceptionType);
+			Assert.assertEquals("Enter Proper Mood", e.getMessage());
+	    }
+	}
+	
+	@Test
+	public void givenLastName_WhenEmpty_ShouldReturnEnterProperMood() {
+	UserRegistration userRegistration = new UserRegistration();
+		try {
+		    boolean result = userRegistration.checkLastName("");
+		    Assert.assertEquals(false,result);
+		}catch(UserRegistrationException e) {
+			Assert.assertEquals(UserRegistrationException.ExceptionType.ENTERED_EMPTY, e.exceptionType);
+			Assert.assertEquals("Enter Proper Mood", e.getMessage());			
+		}
+	}
 		
 }
   
